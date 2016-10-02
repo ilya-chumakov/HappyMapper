7aaf8456cee7c09a0c8a7cc2a9fd380b3ac5f1a2
Chumakov Ilya <aidforsoft@mail.ru>
2016-10-02 22:29:50 +0300

    Test to map list of lists (unsupported yet).

--- OrdinaryMapper.Tests/MapCollections_Tests.cs
+++ OrdinaryMapper.Tests/MapCollections_Tests.cs
@@ -26,6 +26,16 @@ namespace OrdinaryMapper.Tests
             public List<Dest> P1 { get; set; }
         }
 
+        public class SrcWrapLevel2
+        {
+            public List<List<Src>> P1 { get; set; }
+        }
+
+        public class DestWrapLevel2
+        {
+            public List<List<Dest>> P1 { get; set; }
+        }
+
         [Test]
         public void Map_ListIsTarget_Success()
         {
@@ -62,12 +72,45 @@ namespace OrdinaryMapper.Tests
             List<Dest> destList = new List<Dest>();
             destList.Add(new Dest());
 
-            SrcWrap srcWrap = new SrcWrap() { P1 = srcList };
-            DestWrap destWrap = new DestWrap() { P1 = destList };
+            SrcWrap srcWrap = new SrcWrap { P1 = srcList };
+            DestWrap destWrap = new DestWrap { P1 = destList };
 
-            mapper.Map<SrcWrap, DestWrap>(srcWrap, destWrap);
+            mapper.Map(srcWrap, destWrap);
 
             Assert.AreEqual(srcWrap.P1[0].Value1, destWrap.P1[0].Value1);
+        }
+
+        [Test]
+        public void Map_ListOfLists_NotMapped()
+        {
+            var config = new HappyConfig(cfg =>
+            {
+                cfg.CreateMap<Src, Dest>();
+                cfg.CreateMap<SrcWrapLevel2, DestWrapLevel2>();
+            });
+            config.AssertConfigurationIsValid();
+            var mapper = config.CompileMapper();
+
+            var srcList = new List<Src>();
+            var srcListL2 = new List<List<Src>>();
+            srcList.Add(new Src { Value1 = 1 });
+            srcListL2.Add(srcList);
+
+
+            var destList = new List<Dest>();
+            var destListL2 = new List<List<Dest>>();
+            destList.Add(new Dest());
+            destListL2.Add(destList);
+
+            var srcWrap = new SrcWrapLevel2 { P1 = srcListL2 };
+            var destWrap = new DestWrapLevel2 { P1 = destListL2 };
+
+            mapper.Map(srcWrap, destWrap);
+
+            //It's easy to make things work by modifying AssignCollections method: 
+            //dig onto generic arguments in cycle until you met non-collection type at both src and dest ends.
+            //However, I think the destination creation module should be implemented first.
+            Assert.AreNotEqual(srcWrap.P1[0][0].Value1, destWrap.P1[0][0].Value1);
         }
     }
 }
